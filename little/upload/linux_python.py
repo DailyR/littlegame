bdda5769517636ee8a8c0398dd01c6d87d9e3dd7
DailyR <li_dai@163.com>
2019-03-06 10:31:03 +0800

    add djangoblog , as the second blog ,the first is jbt_blog

--- little/upload/linux_python.py
+++ little/upload/linux_python.py
@@ -1,34 +1,9 @@
-#-*- coding:utf-8 -*-  
-#Sample_Python.py  
-#just used for 
-''''' 
-Created on 2015-X-XX 
- 
-@author: daily 
-'''
-#Sample
+from selenium import webdriver
 
 
-import time
-import os
+driver = webdriver.Firefox()
+driver.get('https://www.baidu.com')
 
-def main():
-	'''docstring'''
-	print "hello world!"
-	print time.time()
-	print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-	time_str = time.strftime('%m%d%H%M%y')
-	print time_str
-	os.system('date 1119181423')
-	print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-	os.system('date %s' % time_str) 
-	print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+print(driver.title)
 
-class A_Class(object):
-	def __init__(self):
-		print "it is A_Class!"
-
-
-if __name__ == '__main__':
-	main()
-	a = A_Class()
+driver.quit()
